5a5d02582458ab19af9d9cb9c7f0c79452200f14
Glenn Raya <rayaglenn@gmail.com>
2024-08-31 08:16:05 +0800

    Initial commit - Laravel template demo for YT

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProfileController;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    $users = User::where('id', '!=', Auth::id())->simplePaginate(10);
+
+    return Inertia::render('Dashboard', ['users' => $users]);
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
